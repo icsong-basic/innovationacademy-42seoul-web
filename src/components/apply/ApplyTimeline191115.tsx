92e9dc4ee6af4f9471029eaff734d5a1cc304a0b
icsong-basic <inchul@innovationacademy.kr>
2020-07-07 10:53:14 +0900

    Update ApplyTimeline191115.tsx
    42seoul 일정 수정

--- src/components/apply/ApplyTimeline191115.tsx
+++ src/components/apply/ApplyTimeline191115.tsx
@@ -4,7 +4,7 @@ export default function ApplyTimeline191115() {
     return (
         <>
             <h2 className="title">
-                ※ 42 SEOUL 1기 교육생 모집은 마감되었습니다.<br />
+                ※ 42 SEOUL 2기 교육생 모집은 마감되었습니다.<br />
                 ※ 지금 온라인 테스트를 진행하시면 2, 3기 대기자 등록 또는 그 이후 열리는 체크인 지원이 가능합니다.<br />
                 ※ 추후 체크인 미팅 일정이 오픈되면 온라인 테스트 합격자에게 일정을 안내드립니다.
             </h2>
@@ -16,28 +16,28 @@ export default function ApplyTimeline191115() {
                 </div>
                 <div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.06.11~20.06.12</p>
                         <p className="description">1차 체크인 미팅</p>
                     </div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.07.09~20.07.10</p>
                         <p className="description">2차 체크인 미팅</p>
                     </div>
                     <div className="right-arrow" />
                 </div>
                 <div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.06.29~20.07.24</p>
                         <p className="description">1차 1개월 집중교육<br />(La Piscine)</p>
                     </div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.07.27~20.08.21</p>
                         <p className="description">2차 1개월 집중교육<br />(La Piscine)</p>
                     </div>
                     <div className="right-arrow" />
                 </div>
                 <div>
-                    <p className="description">미정<br />본교육 과정</p>
+                    <p className="description">20.08.31 ~ <br />본교육 과정</p>
                 </div>
             </div>
             <h2>3기</h2>
@@ -48,28 +48,28 @@ export default function ApplyTimeline191115() {
                 </div>
                 <div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.08.13~20.08.14</p>
                         <p className="description">1차 체크인 미팅</p>
                     </div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.09.17~20.09.18</p>
                         <p className="description">2차 체크인 미팅</p>
                     </div>
                     <div className="right-arrow" />
                 </div>
                 <div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.08.31~20.09.25</p>
                         <p className="description">1차 1개월 집중교육<br />(La Piscine)</p>
                     </div>
                     <div className="half">
-                        <p className="period color-mint">미정</p>
+                        <p className="period color-mint">20.10.05~20.10.30</p>
                         <p className="description">2차 1개월 집중교육<br />(La Piscine)</p>
                     </div>
                     <div className="right-arrow" />
                 </div>
                 <div>
-                    <p className="description">미정<br />본교육 과정</p>
+                    <p className="description">20.11.02 ~<br />본교육 과정</p>
                 </div>
             </div>
         </>
