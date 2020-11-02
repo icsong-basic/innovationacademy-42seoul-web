a8b5d5367346c927a48085a5ad849089b52300ba
icsong-basic <inchul@innovationacademy.kr>
2020-11-02 11:04:30 +0900

    Update ApplyTimeline191115.tsx
    일정변경

--- src/components/apply/ApplyTimeline191115.tsx
+++ src/components/apply/ApplyTimeline191115.tsx
@@ -4,9 +4,8 @@ export default function ApplyTimeline191115() {
     return (
         <>
             <h2 className="title">
-                ※ 42 SEOUL 2기 교육생 모집은 마감되었습니다.<br />
-                ※ 지금 온라인 테스트를 진행하시면 2, 3기 대기자 등록 또는 그 이후 열리는 체크인 지원이 가능합니다.<br />
-                ※ 추후 체크인 미팅 일정이 오픈되면 온라인 테스트 합격자에게 일정을 안내드립니다.
+                ※ 42 SEOUL 3기 교육생 모집은 마감되었습니다.<br />
+                ※ 지금 온라인 테스트를 진행하시면 2021년 열리는 체크인 지원이 가능합니다.<br />
             </h2>
             <h2>2기</h2>
             <div className="timeline">
@@ -37,7 +36,7 @@ export default function ApplyTimeline191115() {
                     <div className="right-arrow" />
                 </div>
                 <div>
-                    <p className="description">미정 <br />본교육 과정</p>
+                    <p className="description">20.09.28 ~ <br />본교육 과정</p>
                 </div>
             </div>
             <h2>3기</h2>
@@ -59,17 +58,17 @@ export default function ApplyTimeline191115() {
                 </div>
                 <div>
                     <div className="half">
-                        <p className="period color-mint">20.10.05~20.10.30</p>
+                        <p className="period color-mint">20.10.12~20.11.06</p>
                         <p className="description">1차 1개월 집중교육<br />(La Piscine)</p>
                     </div>
                     <div className="half">
-                        <p className="period color-mint">20.11.09~20.12.04</p>
+                        <p className="period color-mint">20.11.16~20.12.11</p>
                         <p className="description">2차 1개월 집중교육<br />(La Piscine)</p>
                     </div>
                     <div className="right-arrow" />
                 </div>
                 <div>
-                    <p className="description">미정<br />본교육 과정</p>
+                    <p className="description">20.12.21<br />본교육 과정</p>
                 </div>
             </div>
         </>
